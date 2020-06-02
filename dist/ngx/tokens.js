4e60e17bb504cbd79e729b170fc1f3f951f50317
Thomas Trageser <thomast74@gmail.com>
2020-06-02 17:35:56 +0300

    Add tokens for forward compatibility

---
+++ dist/ngx/tokens.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var CancelToken = /** @class */ (function () {
+    function CancelToken() {
+        this.cancelled = false;
+        this._onCancel = function () {
+            console.log('CancelToken: onCancel');
+        };
+    }
+    CancelToken.prototype.cancel = function (onCancel, timeout) {
+        var _this = this;
+        if (timeout === void 0) { timeout = 10; }
+        this._onCancel = onCancel;
+        this.cancelled = true;
+        this.cancelTimeout = setTimeout(function () {
+            _this.onCancel();
+        }, timeout * 1000);
+    };
+    CancelToken.prototype.reset = function () {
+        this.cancelled = false;
+    };
+    CancelToken.prototype.isCancelled = function () {
+        return this.cancelled;
+    };
+    CancelToken.prototype.onCancel = function () {
+        if (this.cancelTimeout) {
+            clearTimeout(this.cancelTimeout);
+        }
+        if (this._onCancel) {
+            this._onCancel();
+        }
+    };
+    return CancelToken;
+}());
+exports.CancelToken = CancelToken;
