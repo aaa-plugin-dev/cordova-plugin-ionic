ffc95545c3631a5d690e29c6333212d51f9c72ae
Thomas Trageser <ttrageser@national.aaa.com>
2021-04-07 14:28:59 +0200

    Update to latest Angular version dependency

--- dist/tokens.js
+++ dist/tokens.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.CancelToken = void 0;
 var CancelToken = /** @class */ (function () {
     function CancelToken() {
         this.cancelled = false;
